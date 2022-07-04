030cacd9b23493c42ef4b4cb9d646cad9378e192
kcalbCube <cubicteamco@gmail.com>
2022-07-05 08:27:56 +1000

    rendering work

---
+++ src/game/game.cpp
@@ -0,0 +1,91 @@
+#include "game.h"
+
+void game::SimpleGame::sendFrame(void)
+{
+	mirage::network::GraphicFrame::SerializedT frame{};
+	mirage::graphics::VerticeGroup vg;
+	auto w = client->windowWidth;
+	auto h = client->windowHeight;
+
+	auto cropX = w - 64 * width;
+	auto cropY = h - 64 * height;
+
+	auto scaleX = static_cast<float>(w) / (width * 64);
+	auto scaleY = static_cast<float>(h) / (height * 64);
+
+	vg.filters.emplace_back(mirage::graphics::Scale(
+		static_cast<unsigned>(scaleX), static_cast<unsigned>(scaleY)));
+	auto icon = mirage::graphics::load("1.png"_hs, "resource/1.png");		
+
+	{
+		auto tile = mirage::graphics::load("tiles/tile.png"_hs, "resource/tiles/tile.png");
+		auto dx = static_cast<unsigned>(64 * scaleX);
+		auto dy = static_cast<unsigned>(64 * scaleY);
+	
+		auto sx = cropX / 2;
+		auto sy = cropY / 2;
+
+		for(uint16_t x = 0; x < width; ++x)
+			for(uint16_t y = 0; y < height; ++y)
+				vg.vertices.emplace_back(
+					sx + x * dx + dx / 2, 
+					sy + y * (dy - 1) + dy / 2, 
+					tile->id);
+	}
+
+	vg.vertices.emplace_back(x, y, icon->id);
+
+
+	frame.push_back(vg);	
+
+	mirage::network::GraphicFrame framePacket;
+	auto serialized = mirage::utils::serialize(frame);
+	memcpy(framePacket.serialized, serialized.c_str(), serialized.size());
+	mirage::network::server::networkController().send(
+		client->getConnection(), 
+		mirage::network::AbstractPacket{framePacket});
+}
+
+void game::SimpleGame::onInput(mirage::network::server::PacketReceivedEvent<mirage::network::Input>& packet)
+{
+	if(packet.username != client->getUsername())
+		return;
+	auto deserialized = mirage::utils::deserialize<mirage::network::Input::SerializedT>(
+		std::string{mirage::utils::span(packet.packet.serialized)});
+
+	bool isUpdated = false;
+	for(auto&& key : deserialized)
+	{
+		switch(key)
+		{
+		case SDL_SCANCODE_W:
+			y -= 32;
+			break;
+		case SDL_SCANCODE_S:
+			y += 32;
+			break;
+		case SDL_SCANCODE_D:
+			x += 32;
+			break;
+		case SDL_SCANCODE_A:
+			x -= 32;
+			break;
+		default: continue;
+		}
+
+		isUpdated = true;
+	}
+
+	if(isUpdated)
+		sendFrame();
+}
+
+void game::SimpleGame::initialize(mirage::server::ClientAuthorizedEvent& event)
+{
+	client = event.client;
+}
+
+void game::SimpleGame::lateInitialize(void)
+{
+	bindEvent<mirage::network::server::PacketReceivedEvent<mirage::network::Input>, &SimpleGame::onInput>();
+}
--- src/game/game.h
+++ src/game/game.h
@@ -17,83 +17,16 @@ namespace game
 	public:
 		mirage::ecs::ComponentWrapper<mirage::server::Client> client;
 
-		float x = 0, y = 0;
+		uint16_t x = 0, y = 0;
+		unsigned width = 30,
+			 height = 16;
 
-		void sendFrame(void)
-		{
-			mirage::network::GraphicFrame::SerializedT frame{};
-			mirage::graphics::VerticeGroup vg;
-			auto w = client->windowWidth;
-			auto h = client->windowHeight;
-			vg.filters.emplace_back(mirage::graphics::Scale(w / (13 * 64), h / (9 * 64)));
-			auto icon = mirage::graphics::load("1.png"_hs, "resource/1.png");	
+		void sendFrame(void);
 
-			vg.vertices.emplace_back(static_cast<uint16_t>(x * UINT16_MAX), static_cast<uint16_t>(y * UINT16_MAX), icon->id);
+		void onInput(mirage::network::server::PacketReceivedEvent<mirage::network::Input>& packet);	
 
-			{
-				auto tile = mirage::graphics::load("tiles/tile.png"_hs, "resource/tiles/tile.png");
-				auto dx = UINT16_MAX / 13;
-				auto dy = UINT16_MAX / 9;
-
-				for(uint16_t x = 0; x < 13; ++x)
-					for(uint16_t y = 0; y < 9; ++y)
-						vg.vertices.emplace_back(x * dx + dx / 2, y * dy + dy / 2, tile->id);
-			}
-
-
-			frame.push_back(vg);	
-
-			mirage::network::GraphicFrame framePacket;
-			auto serialized = mirage::utils::serialize(frame);
-			memcpy(framePacket.serialized, serialized.c_str(), serialized.size());
-			mirage::network::server::networkController().send(
-				client->getConnection(), 
-				mirage::network::AbstractPacket{framePacket});
-		}
-
-		void onInput(mirage::network::server::PacketReceivedEvent<mirage::network::Input>& packet)
-		{
-			if(packet.username != client->getUsername())
-				return;
-			auto deserialized = mirage::utils::deserialize<mirage::network::Input::SerializedT>(
-				std::string{mirage::utils::span(packet.packet.serialized)});
-
-			bool isUpdated = false;
-			for(auto&& key : deserialized)
-			{
-				switch(key)
-				{
-				case SDL_SCANCODE_W:
-					y -= 0.1f;
-					break;
-				case SDL_SCANCODE_S:
-					y += 0.1f;
-					break;
-				case SDL_SCANCODE_D:
-					x += 0.1f;
-					break;
-				case SDL_SCANCODE_A:
-					x -= 0.1f;
-					break;
-				default: continue;
-				}
-
-				isUpdated = true;
-			}
-
-			if(isUpdated)
-				sendFrame();
-		}
-
-		void initialize(mirage::server::ClientAuthorizedEvent& event)
-		{
-			client = event.client;
-		}
-
-		void lateInitialize(void)
-		{
-			bindEvent<mirage::network::server::PacketReceivedEvent<mirage::network::Input>, &SimpleGame::onInput>();
-		}
+		void initialize(mirage::server::ClientAuthorizedEvent& event);	
+		void lateInitialize(void);
 	};
 	MIRAGE_CREATE_WITH_EVENT(
 		mirage::server::ClientAuthorizedEvent,
